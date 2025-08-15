624ca6e7db7be06c2ad403af46100a8c997207fc
Arpit Jain <YOUR_GITHUB_EMAIL>
2025-08-15 21:35:25 +0530

    Updated site

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out',
   trailingSlash: true,
+  skipTrailingSlashRedirect: true,
   images: {
     unoptimized: true,
   },
-  basePath: '/jainpandithyderabad',
-  assetPrefix: '/jainpandithyderabad/',
+  basePath: process.env.NODE_ENV === 'production' ? '/jainpandithyderabad' : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/jainpandithyderabad/' : '',
 };
 
 export default nextConfig;
