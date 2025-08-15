9263b6f743727be94a1c8b5f71ef756723e7623a
Arpit Jain <YOUR_GITHUB_EMAIL>
2025-08-15 18:54:51 +0530

    Update site

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  // This enables static export
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   images: {
-    unoptimized: true // Avoids Next.js image optimization server
-  }
-};
+    unoptimized: true,
+  },
+}
 
 export default nextConfig;
