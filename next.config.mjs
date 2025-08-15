bffaced085c368d3179635054188304ab7422e68
Arpit Jain <71266987+Arpit-Jain0@users.noreply.github.com>
2025-08-15 19:39:50 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export',
   basePath: '/jainpandithyderabad',
   assetPrefix: '/jainpandithyderabad/',
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
