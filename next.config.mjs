492fe1ba5fb820fdb3ece4a0a3f9750a369ee5d4
Arpit Jain <YOUR_GITHUB_EMAIL>
2025-08-15 22:37:12 +0530

    Updated site

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,7 @@ const nextConfig = {
   trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  // Remove these lines for custom domain:
-  // basePath: '/jainpandithyderabad',
-  // assetPrefix: '/jainpandithyderabad/',
+  }
 };
 
 export default nextConfig;
